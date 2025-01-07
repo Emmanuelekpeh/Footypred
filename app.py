88d5a807eabf505b16738c66efbac378262e316c
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-01-07 10:33:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,27 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the model
-model = joblib.load('best_model.pkl')
+# Load the models
+model_o2_5 = joblib.load('model_o2_5.pkl')
+model_result = joblib.load('model_result.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     features = pd.DataFrame([data['features']])
-    prediction = model.predict(features)
-    return jsonify({'prediction': prediction[0]})
+    prediction_o2_5 = model_o2_5.predict(features)
+    prediction_result = model_result.predict(features)
+    
+    # Include the original input data (team names and date) in the response
+    response = {
+        'prediction_o2_5': prediction_o2_5[0],
+        'prediction_result': prediction_result[0],
+        'home_team': data['features']['HomeTeam'],
+        'away_team': data['features']['AwayTeam'],
+        'date': data['features']['Date']
+    }
+    
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
